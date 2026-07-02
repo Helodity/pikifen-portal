af99b475315a35b9643af97f45c4bb4ff3ed3ae8
Helodity <kbartlettofficial@gmail.com>
2026-07-02 11:17:22 -0400

    Move deployment specific variables to ignored file

--- includes/globals.php
+++ includes/globals.php
@@ -1,6 +1,10 @@
 <?php
+//Contains definition for $PATH_FROM_ROOT
+include "deployment_globals.php";
+
 $SITE_NAME = "Pikifen Portal";
-$SITE_ROOT = "https://localhost/pikifen"; //Use when outputting HTML
-$LOCAL_ROOT = $_SERVER['DOCUMENT_ROOT'] . '/pikifen'; //Use when processing
+$SITE_ROOT = 'https://' . $_SERVER['HTTP_HOST'] . $PATH_FROM_ROOT; //Use when outputting HTML;
+$LOCAL_ROOT = $_SERVER['DOCUMENT_ROOT'] . $PATH_FROM_ROOT; //Use when processing
+
 $NEWEST_ENGINE_VERSION_STR = "1.1.1";
 ?>
